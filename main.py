158df1b8e86ed5ff9452f7edb7fa1c627a5c755d
eyobScript <eyobbmulugeta@gmail.com>
2025-03-08 10:03:33 +0300

    Python Live Chat Room First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, session, request, redirect
+from flask_socketio import join_room, leave_room, SocketIO, send
+import random
+from string import ascii_uppercase
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'gdfvbnsoehkcxdtfcygvuhijofebcw'
+socketio = SocketIO(app=app)
+
+
+
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
